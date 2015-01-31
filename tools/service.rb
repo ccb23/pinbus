09a8923784ecafd1fd96c4937dfc04c9f2b761c2
root <root@raspberrypi.(none)>
2015-02-01 00:53:29 +0100

    working service :-)

--- tools/service.rb
+++ tools/service.rb
@@ -12,15 +12,18 @@ Net::HTTP.start(uri.host, uri.port) do |http|
   STDIN.each_byte do |char|
     if char == 255
       unless @data.empty?
+	if @data[0] == '05' and @data[1] == '0c'
+	  print @data[2..-1].join('') + '  -  '
+	end
         query = { 'event[data][]' => @data, 'event[time]' => Time.now.to_i }
         response = http.post(uri.path, URI.encode_www_form(query))
-        puts response.body
-
+        puts response.code #body
+ 
         @data = []
       end
     else
       @data << sprintf("%.2x", char)
     end
-    sleep 0.05
+    sleep 0.02
   end
 end
